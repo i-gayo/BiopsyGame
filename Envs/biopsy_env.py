Issue #6 : fixed misalignment problem by removing bugs in centroid calculation

Envs/biopsy_env.py
@@ -540,11 +540,14 @@ class TemplateGuidedBiopsy(gym.Env):
       y_idx = current_pos[1]#*5
 
       #Converts range from (-30,30) to image grid array
-      x_idx = (x_idx) + 50
-      y_idx = (y_idx) + 50
+      # before : add +50 to each x and y position
+      
+      x_idx = (x_idx) + round(self.prostate_centroid[0]/2)
+      y_idx = (y_idx) + round(self.prostate_centroid[1]/2)
 
-      x_grid_pos = int(x_idx)
-      y_grid_pos = int(y_idx)
+      x_grid_pos = round(x_idx)
+      y_grid_pos = round(y_idx)
+      #print(f"ENV x and y:  {x_grid_pos} and {y_grid_pos}")
 
       depth_map = {0 : 1, 1 : int(0.5*self.max_depth), 2 : self.max_depth}
       depth = depth_map[int(current_pos[2])]
@@ -653,6 +656,7 @@ class TemplateGuidedBiopsy(gym.Env):
 
         #Obtain bounding box of prostate, tumour masks (for tumour this is a bounding sphere) 
         self.bb_prostate_mask, self.prostate_centroid = self._extract_volume_params(prostate_mask, which_case= 'Prostate')
+        print(f"Prostate centroid : {self.prostate_centroid}")
         self.max_needle_depth = np.max(np.where(self.img_data['prostate_mask'] == 1)[-1]) #max z depth with prostate present using whole volume 
         self.max_depth = int(self.max_needle_depth /4) # downsamples image volume so / 4 
         #self.max_depth = np.max(np.where(self.img_data['prostate_mask'][::2,::2,::4] == 1)[-1]) #max z depth with prostate present
@@ -933,9 +937,11 @@ class TemplateGuidedBiopsy(gym.Env):
       x_movement = round_to_05(action_x * max_step_size)
       y_movement = round_to_05(action_y * max_step_size)
 
+
       updated_x = self.current_needle_pos[0] + x_movement
       updated_y = self.current_needle_pos[1] + y_movement
 
+      print(f"Current grid pos : {self.current_needle_pos} new pos : ({updated_x},{updated_y})")
       #Dealing with boundary positions 
       x_lower = updated_x < -30
       x_higher = updated_x > 30

implement_game.py
@@ -21,6 +21,36 @@ def generate_grid(prostate_centroid):
     x_grid = (np.arange(-30,35,5))*2 + prostate_centroid[0]
     y_grid = (np.arange(-30,35,5))*2 + prostate_centroid[1]
 
+    grid = np.zeros((100,100))
+    for i in range(-30, 35, 5):
+        for j in range(-30, 35, 5):
+            x_val = round(prostate_centroid[1]/2)+j
+            y_val = round(prostate_centroid[0]/2) +i
+            
+            grid[y_val - 1:y_val+2 , x_val -1 : x_val+2] = 1
+
+    grid_coords = np.array(np.where(grid == 1))  # given in y, x 
+    
+    # change to x,y convention instead of y,x 
+    grid_coords[[0,1],:] = grid_coords[[1,0],:]
+    
+    return grid, grid_coords
+
+
+def generate_grid_old(prostate_centroid):
+    """
+    Generates 2D grid of grid point coords on image coordinates
+    
+    Arguments:
+    :prostate_centroid (ndarray) : centroid in x,y,z convention of prostate gland 
+    
+    Returns:
+    :grid_coords (ndarray) : 2 x 169 grid coords x,y convention 
+    """
+    
+    x_grid = (np.arange(-30,35,5))*2 + prostate_centroid[0]
+    y_grid = (np.arange(-30,35,5))*2 + prostate_centroid[1]
+
     grid = np.zeros((200,200))
     for i in range(-60, 65, 10):
         for j in range(-60, 65, 10):
@@ -36,6 +66,7 @@ def generate_grid(prostate_centroid):
     
     return grid, grid_coords
     
+    
 if __name__ == '__main__':
     
     ps_path = '/Users/ianijirahmae/Documents/DATASETS/Data_by_modality'
@@ -50,7 +81,7 @@ if __name__ == '__main__':
     # HYPERPARAMETERS
     RATE = 0.1
     SCALE = 0.25
-    NUM_EPISODES = 2
+    NUM_EPISODES = 5
     
     #### 1. Load biopsy env ####
     biopsy_env = TemplateGuidedBiopsy(Data_sampler,results_dir = 'game', reward_fn = 'reward', \
@@ -61,8 +92,24 @@ if __name__ == '__main__':
     agent = PPO(CnnPolicy, env = biopsy_env, policy_kwargs = policy_kwargs)
     
     ### 3. User interface     ####
+    # obs = biopsy_env.reset()
+    # obs = biopsy_env.reset()
+    # obs = biopsy_env.reset()
+    # obs = biopsy_env.reset()
+    # obs = biopsy_env.reset()
+    # obs = biopsy_env.reset()
+    # obs = biopsy_env.reset()
+    # obs = biopsy_env.reset()
+    # obs = biopsy_env.reset()
+    # obs = biopsy_env.reset()
+    
+    #TODO: fix bug in maybe round_to_05 unction? 
     for i in range(NUM_EPISODES):
         
+        # reseset twice
+        #if i == 0:
+        #    obs = biopsy_env.reset()
+
         obs = biopsy_env.reset()
         vols = biopsy_env.get_img_data()
         done = False 
@@ -75,6 +122,7 @@ if __name__ == '__main__':
             mri_vol = vols['mri_vol']
             prostate_vol = vols['prostate_mask']
             prostate_centroid = np.mean(np.where(prostate_vol), axis = 1)
+            print(f"Game rostate centroid : {prostate_centroid}")
             SLICE_NUM = int(prostate_centroid[-1])
             
             # Define grid coords 
@@ -92,27 +140,29 @@ if __name__ == '__main__':
             mask_n_2= np.ma.array(obs[-3,:,:,:].numpy(), mask=(obs[-3,:,:,:].numpy()==0.0))
             mri_ds = mri_vol[::2,::2,::4]
             needle = np.ma.array(grid, mask = (grid == 0.0))
-            needle_ds = needle[::2,::2]
+            #needle_ds = needle[::2,::2]
             x_cent = int(prostate_centroid[1]/2)
             y_cent = int(prostate_centroid[0]/2)
             
             # crop between y_cent-35:y_cent+30, x_cent-30:x_cent+40; but user input neext to select grid positions within [100,100]
             plt.imshow(mri_ds[:,:, int(SLICE_NUM/4)], cmap ='gray')
-            plt.imshow(50*needle_ds[:,:], cmap='jet', alpha = 0.5)
+            plt.imshow(50*needle[:,:], cmap='jet', alpha = 0.5)
             plt.imshow(np.max(mask_p[:,:,:], axis =2),cmap='coolwarm_r', alpha=0.5)
             plt.imshow(np.max(mask_n_1[:,:,:], axis =2),cmap='Wistia', alpha=0.4)
             plt.imshow(np.max(mask_n_2[:,:,:], axis =2),cmap='Wistia', alpha=0.4)
-            plt.imshow(50*needle_ds[:,:], cmap='jet', alpha = 0.3)
+            plt.imshow(50*needle[:,:], cmap='jet', alpha = 0.3)
             plt.imshow(np.max(mask_l[:,:,:], axis =2),cmap='summer', alpha=0.6)
             plt.imshow(np.max(mask_n[:,:,:], axis =2),cmap='Wistia', alpha=0.5)
             
             # ADDING labels to grid positions!!!
-            first_x = np.min(np.where(grid == 1)[1])/2
-            first_y = np.min(np.where(grid == 1)[0])/2
-            last_x = np.max(np.where(grid == 1)[1])/2
-            s = 'A  a  B  b  C  c  D  d  E  e  F  f  G'
-            plt.text(first_x, first_y - 5, s, fontsize = 10.5, color = 'aqua', bbox=dict(fill=False, edgecolor='green', linewidth=1))#, transform= axs.transAxes)
+            first_x = np.min(np.where(grid == 1)[1])
+            first_y = np.min(np.where(grid == 1)[0])
+            last_x = np.max(np.where(grid == 1)[1])
+            s = 'A  a  B  b  C  c  D  d  E  e  F  f  G' # fontsize 10.5 
+            #s = '-30 -25 -20 -15 -10 -5 0 5 10 15 20 25 30' #font size 8
+            plt.text(first_x, first_y - 5, s, fontsize = 8, color = 'aqua', bbox=dict(fill=False, edgecolor='green', linewidth=1))#, transform= axs.transAxes)
             grid_labels = np.arange(7, 0.5, -0.5)
+            grid_labels = np.arange(-30, 35, 5)
             for idx, label in enumerate(grid_labels):
                 plt.text(first_x-10, first_y + (idx*5.15), label, fontsize = 10.5, color = 'aqua')
                 plt.text(last_x+5, first_y + (idx*5.15), label, fontsize = 10.5, color = 'aqua')
@@ -120,10 +170,12 @@ if __name__ == '__main__':
             plt.axis('off')
             
             ### 4. Take in user actions to implement strategy ###
-            grid_pos = plt.ginput(0,0)     
-            grid_pos = np.array(grid_pos[0])
-            grid_pos -= (prostate_centroid/2)[0:-1]
-            
+            grid_pos = plt.ginput(1,0) #0,0)     
+            #grid_pos = round_to_05(np.array(grid_pos[0]))
+            prostate_cent_xy = np.array([prostate_centroid[1], prostate_centroid[0]])/2
+            grid_pos -= ((prostate_cent_xy))
+
+        
             # Swap x and y 
             #grid_pos[0], grid_pos[1] = grid_pos[1], grid_pos[0]
             
@@ -134,8 +186,12 @@ if __name__ == '__main__':
                 current_pos = biopsy_env.get_current_pos()
             
             #grid_pos = np.swapaxes(grid_pos, 1, 0)
-            taken_actions = round_to_05(grid_pos - current_pos) 
-            taken_actions = taken_actions / 10 # normalise between (-1,1)       
+
+            raw_actions = np.round(grid_pos - current_pos)
+            taken_actions = round_to_05(np.round(grid_pos - current_pos))
+
+            taken_actions = taken_actions / 10 # normalise between (-1,1)  
+            #taken_actions[0], taken_actions[1] = taken_actions[1], taken_actions[0]     
             taken_actions = np.append(taken_actions, ([1]))                                                                                         
             
             # Take step in environment 